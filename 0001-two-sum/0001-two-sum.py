afbb1bdc43dda71807a35cb523c727e7ae08393c
TarunChoudhury <166103989+TarunChoudhury@users.noreply.github.com>
2024-04-30 15:26:50 +0530

    Time: 59 ms (65.57%), Space: 17.6 MB (56.1%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+        return []
+
+# Test cases
+assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
+assert Solution().twoSum([3, 2, 4], 6) == [1, 2]
+assert Solution().twoSum([3, 3], 6) == [0, 1]
